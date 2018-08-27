b4f1d8db99da997d815519162bb1e1e3da9315c2
liyangai <1297742315@qq.com>
2018-08-27 14:27:07 +0800

    初始化完成

--- app/app.js
+++ app/app.js
@@ -1,3 +1,12 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
+var app = angular.module('moviecat',['ngRoute','moviecat.movie_detail','moviecat.movieList','moviecat.directives.auto_focus']);
+app.constant('AppConfig',{
+    pageSize: 10,
+    listApiAddress: 'http://api.douban.com/v2/movie/',
+    detailApiAddress: 'http://api.douban.com/v2/movie/subject/'
+});
+app.config(['$routeProvider',function ($routeProvider) {
+    $routeProvider.otherwise({redirectTo: '/in_theaters/1'});
+}])
--- app/components/auto-focus.js
+++ app/components/auto-focus.js
@@ -15,10 +15,12 @@
         link: function($scope, iElm, iAttrs, controller) {
 
           $scope.$location = $location;
-          $scope.$watch('$location.path()', function(now) {
+          $scope.$watch('$location.$$path', function(now) {
             // 当path发生变化时执行，now是变化后的值
+         
             var aLink = iElm.children().attr('href');
             var type = aLink.replace(/#(\/.+?)\/\d+/, '$1'); // /coming_soon
+           
             if (now.startsWith(type)) {
               // 访问的是当前链接
               iElm.parent().children().removeClass('active');
